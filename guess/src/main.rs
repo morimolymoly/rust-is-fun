727e1b405b937f6ca3a46613c2ad892a37ee927d
morimolymoly <morimolymoly@gmail.com>
2018-07-18 16:00:14 +0900

    add: pattern match

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main(){
@@ -15,5 +16,15 @@ fn main(){
 
     io::stdin().read_line(&mut guess)
         .expect("読み込み失敗!");
+
+    let guess: u32 = guess.trim().parse()
+        .expect("数字を入力してください!");
+
     println!("予想した数字: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("小さスギィッ!!"),
+        Ordering::Greater => println!("イキスギィッ!!"),
+        Ordering::Equal => println!("正解!!"),
+    }
 }
