95a4593def865fc1e5ecd2e38e9cd3656ea51cd6
morimolymoly <morimolymoly@gmail.com>
2018-07-18 16:02:43 +0900

    add: enable loooooop

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -9,22 +9,27 @@ fn main(){
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("秘密の数字: {}", secret_number);
+    loop {
+        println!("予想を入力して！");
 
-    println!("予想を入力して！");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("読み込み失敗!");
 
-    io::stdin().read_line(&mut guess)
-        .expect("読み込み失敗!");
+        let guess: u32 = guess.trim().parse()
+            .expect("数字を入力してください!");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("数字を入力してください!");
+        println!("予想した数字: {}", guess);
 
-    println!("予想した数字: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("小さスギィッ!!"),
-        Ordering::Greater => println!("イキスギィッ!!"),
-        Ordering::Equal => println!("正解!!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("小さスギィッ!!"),
+            Ordering::Greater => println!("イキスギィッ!!"),
+            Ordering::Equal => {
+                println!("正解!!");
+                break;
+            }
+        }
     }
+    println!("完")
 }
