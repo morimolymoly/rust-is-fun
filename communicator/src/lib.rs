e39a31f091cc5035114de48037a41923fee1f613
morimolymoly <morimolymoly@gmail.com>
2018-08-22 22:40:05 +0900

    update communicator

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -3,8 +3,11 @@ pub mod network;
 
 #[cfg(test)]
 mod tests {
+    use super::client;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        ::client::connect();
+        super::client::connect();
+        client::connect();
     }
 }
