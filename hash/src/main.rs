6cb4686be7982c0639f9b651b874a1dd40ec9cf6
morimolymoly <morimolymoly@gmail.com>
2018-08-25 05:58:44 +0900

    hash

---
+++ hash/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    use std::collections::HashMap;
+
+    let mut scores: HashMap<String, u32> = HashMap::new();
+    scores.insert(String::from("電通大"), 10);
+    scores.insert(String::from("でんとぅー大"), 100);
+
+    println!("Hello, world!");
+}
