53a10119e067443f507570d98cb25b1e0079adc5
morimolymoly <morimolymoly@gmail.com>
2018-10-03 11:38:02 +0900

    trait

--- longest/src/main.rs
+++ longest/src/main.rs
@@ -1,10 +1,13 @@
 fn main() {
     let s = String::from("long");
     let s2 = String::from("longlong");
-    println!("{}", longest(s.as_str(), s2.as_str()));
+    println!("{}", longest(s.as_str(), s2.as_str(), "fuck you"));
 }
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+fn longest<'a, T>(x: &'a str, y: &'a str, msg: T) -> &'a str
+    where T: std::fmt::Display
+{
+    println!("{}", msg);
     if x.len() > y.len() {
         x
     } else {
